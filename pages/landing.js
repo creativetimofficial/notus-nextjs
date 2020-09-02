0a45c58d38d30f527ea2d56ab5392e74016c82c7
EINazare <emanuelioannazare@gmail.com>
2020-09-02 17:59:53 +0300

    fix: solve link not imported error

--- pages/landing.js
+++ pages/landing.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from "next/link";
+
+// components
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
