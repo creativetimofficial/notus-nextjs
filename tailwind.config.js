e8fc8030ed4bbae59554ecd166e3dfcf893c647f
EINazare <emanuelioannazare@gmail.com>
2020-09-02 15:43:56 +0300

    fix: solve paths for tailwind compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,10 @@ module.exports = {
   purge: {
     enabled: true,
     content: [
-      "./public/**/*.html",
-      "./public/*.html",
-      "./src/**/*.js",
-      "./src/*.js",
-      "./src/**/*.html",
-      "./src/*.html",
-      "./public/**/*.js",
-      "./public/*.js",
+      "./**/*.html",
+      "./*.html",
+      "./**/*.js",
+      "./*.js"
     ],
     options: {
       whitelist: [],
