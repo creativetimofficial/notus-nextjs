de7d2c27a735f3a215dd4aa5271b4d0fb9cd540e
EINazare <emanuelioannazare@gmail.com>
2020-09-02 16:14:56 +0300

    refactor: add page change animation

--- pages/_app.js
+++ pages/_app.js
@@ -4,27 +4,27 @@ import App from "next/app";
 import Head from "next/head";
 import Router from "next/router";
 
-// import PageChange from "components/PageChange/PageChange.js";
+import PageChange from "components/PageChange/PageChange.js";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
-// Router.events.on("routeChangeStart", (url) => {
-//   console.log(`Loading: ${url}`);
-//   document.body.classList.add("body-page-transition");
-//   ReactDOM.render(
-//     <PageChange path={url} />,
-//     document.getElementById("page-transition")
-//   );
-// });
-// Router.events.on("routeChangeComplete", () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-//   document.body.classList.remove("body-page-transition");
-// });
-// Router.events.on("routeChangeError", () => {
-//   ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-//   document.body.classList.remove("body-page-transition");
-// });
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+    <PageChange path={url} />,
+    document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
 
 export default class MyApp extends App {
   componentDidMount() {
