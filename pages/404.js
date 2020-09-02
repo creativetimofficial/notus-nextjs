a391a4bb396154f8a99a3154015afb48bf0c4b20
EINazare <emanuelioannazare@gmail.com>
2020-09-02 17:05:14 +0300

    style: run prettier

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,10 @@ import Router from "next/router";
 
 export default class Error404 extends Component {
   componentDidMount = () => {
-    Router.push((process.env.NODE_ENV === "production"
-      ? "/tailwind-webapp-nextjs"
-      : "")+"/admin/dashboard");
+    Router.push(
+      (process.env.NODE_ENV === "production" ? "/tailwind-webapp-nextjs" : "") +
+        "/admin/dashboard"
+    );
   };
 
   render() {
