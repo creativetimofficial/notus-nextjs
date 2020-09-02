97e7425cc01420b7eb2bd974e5d3c6e8210d2051
EINazare <emanuelioannazare@gmail.com>
2020-09-02 15:27:21 +0300

    fix: solve react-router-dom issue

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
 
 // components
 
@@ -8,14 +7,7 @@ import Sidebar from "components/Sidebar/Sidebar.js";
 import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 
-// views
-
-import Dashboard from "views/admin/Dashboard.js";
-import Maps from "views/admin/Maps.js";
-import Settings from "views/admin/Settings.js";
-import Tables from "views/admin/Tables.js";
-
-export default function Admin() {
+export default function Admin({ children }) {
   return (
     <>
       <Sidebar />
@@ -24,12 +16,7 @@ export default function Admin() {
         {/* Header */}
         <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
-          <Switch>
-            <Route path="/admin/dashboard" component={Dashboard} />
-            <Route path="/admin/maps" component={Maps} />
-            <Route path="/admin/settings" component={Settings} />
-            <Route path="/admin/tables" component={Tables} />
-          </Switch>
+          {children}
           <FooterAdmin />
         </div>
       </div>
--- layouts/Auth.js
+++ layouts/Auth.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
 
 // components
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import FooterSmall from "components/Footers/FooterSmall.js";
 
-// views
-
-import Login from "views/auth/Login.js";
-import Register from "views/auth/Register.js";
-
-export default function Auth() {
+export default function Auth({ children }) {
   return (
     <>
       <Navbar transparent />
@@ -26,10 +20,7 @@ export default function Auth() {
               backgroundRepeat: "no-repeat",
             }}
           ></div>
-          <Switch>
-            <Route path="/auth/login" component={Login} />
-            <Route path="/auth/register" component={Register} />
-          </Switch>
+          {children}
           <FooterSmall absolute />
         </section>
       </main>
