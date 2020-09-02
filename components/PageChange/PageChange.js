29b9392ab0841a46e560e339cf0a1f063d206800
EINazare <emanuelioannazare@gmail.com>
2020-09-02 16:42:00 +0300

    fix: solve class error

--- components/PageChange/PageChange.js
+++ components/PageChange/PageChange.js
@@ -18,7 +18,7 @@ export default function PageChange(props) {
       <div className="top-0 left-0 w-full h-full block z-50 absolute bg-black bg-opacity-50"></div>
       <div className="my-32 mx-auto max-w-sm text-center relative z-50 top-0">
         <div className="block mb-4">
-          <i class="fas fa-circle-notch animate-spin text-white mx-auto text-6xl"></i>
+          <i className="fas fa-circle-notch animate-spin text-white mx-auto text-6xl"></i>
         </div>
         <h4 className="text-lg font-medium text-white">
           Loading page contents for: {props.path}
