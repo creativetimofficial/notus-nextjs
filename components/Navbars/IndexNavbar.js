a89e93ffe8ef4fa72a6c405af7deb1e842e6401f
EINazare <emanuelioannazare@gmail.com>
2020-09-07 18:52:01 +0300

    refactor: add share links

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -52,7 +52,8 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <a
                   className="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Ftailwind-webapp-nextjs%2F"
+                  target="_blank"
                 >
                   <i className="text-gray-500 fab fa-facebook text-lg leading-lg " />
                   <span className="lg:hidden inline-block ml-2">Share</span>
@@ -62,7 +63,8 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <a
                   className="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
+                  href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Ftailwind-webapp-nextjs%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwindcss%20and%20NextJS%20Admin%20and%20Presentation%20Starter%20Kit%20Template.%20Let%20Tailwind%20Webapp%20NextJS%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level.%20"
+                  target="_blank"
                 >
                   <i className="text-gray-500 fab fa-twitter text-lg leading-lg " />
                   <span className="lg:hidden inline-block ml-2">Tweet</span>
@@ -72,7 +74,8 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <a
                   className="hover:text-gray-600 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#pablo"
+                  href="https://github.com/creativetimofficial/tailwind-webapp-nextjs"
+                  target="_blank"
                 >
                   <i className="text-gray-500 fab fa-github text-lg leading-lg " />
                   <span className="lg:hidden inline-block ml-2">Star</span>
