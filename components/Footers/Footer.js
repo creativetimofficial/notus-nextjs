77c0eda4b11a8bc8a0fef1ddfc4848f0d959f040
EINazare <emanuelioannazare@gmail.com>
2020-09-07 18:50:02 +0300

    fix: solve responsive issues

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -24,13 +24,13 @@ export default function Footer() {
           </svg>
         </div>
         <div className="container mx-auto px-4">
-          <div className="flex flex-wrap">
+          <div className="flex flex-wrap text-center lg:text-lef">
             <div className="w-full lg:w-6/12 px-4">
               <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
               <h5 className="text-lg mt-0 mb-2 text-gray-700">
                 Find us on any of these platforms, we respond 1-2 business days.
               </h5>
-              <div className="mt-6">
+              <div className="mt-6 lg:mb-0 mb-6">
                 <button
                   className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                   type="button"
