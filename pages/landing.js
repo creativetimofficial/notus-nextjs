afbc3c0e481de638dba7ffcd3aeb07c11685608d
EINazare <emanuelioannazare@gmail.com>
2020-09-23 16:23:24 +0300

    refactor: change links and names

--- pages/landing.js
+++ pages/landing.js
@@ -34,7 +34,7 @@ export default function Landing() {
                   <p className="mt-4 text-lg text-gray-300">
                     This is a simple example of a Landing Page you can build
                     using Tailwind WebApp NextJS. It features multiple CSS
-                    components based on the Tailwindcss design system.
+                    components based on the Tailwind CSS design system.
                   </p>
                 </div>
               </div>
