125fcb81ce055d751b6e0cda352cd8d9f1ed250a
EINazare <emanuelioannazare@gmail.com>
2020-10-07 11:57:57 +0300

    style: run prettier

--- pages/landing.js
+++ pages/landing.js
@@ -33,8 +33,8 @@ export default function Landing() {
                   </h1>
                   <p className="mt-4 text-lg text-gray-300">
                     This is a simple example of a Landing Page you can build
-                    using Notus NextJS. It features multiple CSS
-                    components based on the Tailwind CSS design system.
+                    using Notus NextJS. It features multiple CSS components
+                    based on the Tailwind CSS design system.
                   </p>
                 </div>
               </div>
