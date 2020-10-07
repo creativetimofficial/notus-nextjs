0989afa6d8dfb87cf7b38abdb0dc623665fe9f73
EINazare <emanuelioannazare@gmail.com>
2020-10-07 11:57:07 +0300

    refactor: change names, links and refs

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -67,7 +67,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://www.creative-tim.com/presentation?ref=twnjs-footer"
+                        href="https://www.creative-tim.com/presentation?ref=nnjs-footer"
                       >
                         About Us
                       </a>
@@ -75,7 +75,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://blog.creative-tim.com?ref=twnjs-footer"
+                        href="https://blog.creative-tim.com?ref=nnjs-footer"
                       >
                         Blog
                       </a>
@@ -83,7 +83,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://www.github.com/creativetimofficial?ref=twnjs-footer"
+                        href="https://www.github.com/creativetimofficial?ref=nnjs-footer"
                       >
                         Github
                       </a>
@@ -91,7 +91,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://www.creative-tim.com/bootstrap-themes/free?ref=twnjs-footer"
+                        href="https://www.creative-tim.com/bootstrap-themes/free?ref=nnjs-footer"
                       >
                         Free Products
                       </a>
@@ -106,7 +106,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://github.com/creativetimofficial/tailwind-webapp-nextjs/blob/master/LICENSE.md?ref=twnjs-footer"
+                        href="https://github.com/creativetimofficial/notus-nextjs/blob/master/LICENSE.md?ref=nnjs-footer"
                       >
                         MIT License
                       </a>
@@ -114,7 +114,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/terms?ref=twnjs-footer"
+                        href="https://creative-tim.com/terms?ref=nnjs-footer"
                       >
                         Terms & Conditions
                       </a>
@@ -122,7 +122,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/privacy?ref=twnjs-footer"
+                        href="https://creative-tim.com/privacy?ref=nnjs-footer"
                       >
                         Privacy Policy
                       </a>
@@ -130,7 +130,7 @@ export default function Footer() {
                     <li>
                       <a
                         className="text-gray-700 hover:text-gray-900 font-semibold block pb-2 text-sm"
-                        href="https://creative-tim.com/contact-us?ref=twnjs-footer"
+                        href="https://creative-tim.com/contact-us?ref=nnjs-footer"
                       >
                         Contact Us
                       </a>
@@ -144,9 +144,9 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-600 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Tailwind WebApp NextJS by{" "}
+                Copyright © {new Date().getFullYear()} Notus NextJS by{" "}
                 <a
-                  href="https://www.creative-tim.com?ref=twnjs-footer"
+                  href="https://www.creative-tim.com?ref=nnjs-footer"
                   className="text-gray-600 hover:text-gray-900"
                 >
                   Creative Tim
