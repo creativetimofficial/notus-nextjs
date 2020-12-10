08778c19cd7712214c99356a9be3218e7c0f0ff7
EINazare <emanuelioannazare@gmail.com>
2020-12-10 09:51:34 +0200

    fix: solve download color issues

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -91,7 +91,7 @@ export default function Navbar(props) {
 
               <li className="flex items-center">
                 <a
-                  className="inline-block text-white bg-pink-500 active:bg-pink-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  className="inline-block text-white bg-gray-800 active:bg-gray-700 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   href="https://www.creative-tim.com/product/notus-nextjs?ref=nnjs-auth"
                   target="_blank"
                 >
