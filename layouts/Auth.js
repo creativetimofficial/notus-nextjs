142d10047a234aba3663a60aa168297257651fb2
EINazare <emanuelioannazare@gmail.com>
2021-03-18 10:42:10 +0200

    refactor: update all

--- layouts/Auth.js
+++ layouts/Auth.js
@@ -12,7 +12,7 @@ export default function Auth({ children }) {
       <main>
         <section className="relative w-full h-full py-40 min-h-screen">
           <div
-            className="absolute top-0 w-full h-full bg-gray-900 bg-no-repeat bg-full"
+            className="absolute top-0 w-full h-full bg-coolGray-800 bg-no-repeat bg-full"
             style={{
               backgroundImage:
                 "url(" + require("assets/img/register_bg_2.png") + ")",
