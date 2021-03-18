dc509aadd58f291b193da6470d6981110fee2474
EINazare <emanuelioannazare@gmail.com>
2021-03-18 18:26:53 +0200

    fix: solve broken colors

--- components/Cards/CardStats.js
+++ components/Cards/CardStats.js
@@ -61,7 +61,7 @@ CardStats.defaultProps = {
   statTitle: "350,897",
   statArrow: "up",
   statPercent: "3.48",
-  statPercentColor: "text-green-500",
+  statPercentColor: "text-emerald-500",
   statDescripiron: "Since last month",
   statIconName: "far fa-chart-bar",
   statIconColor: "bg-red-500",
