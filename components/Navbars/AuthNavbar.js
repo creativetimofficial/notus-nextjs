a92e860413e06552159bdfe1b8f7fe334a4e1c88
EINazare <emanuelioannazare@gmail.com>
2021-03-18 17:39:04 +0200

    fix: solve broken colors

--- components/Navbars/AuthNavbar.js
+++ components/Navbars/AuthNavbar.js
@@ -84,7 +84,7 @@ export default function Navbar(props) {
 
               <li className="flex items-center">
                 <button
-                  className="bg-white text-coolGray-700 active:bg-coolGray-new text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  className="bg-white text-coolGray-700 active:bg-coolGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
                 >
                   <i className="fas fa-arrow-alt-circle-down"></i> Download
