b762843a08ba4bc7a38b46893254e0c87152884f
EINazare <emanuelioannazare@gmail.com>
2021-03-18 10:41:33 +0200

    refactor: update all

--- components/Cards/CardLineChart.js
+++ components/Cards/CardLineChart.js
@@ -108,11 +108,11 @@ export default function CardLineChart() {
   }, []);
   return (
     <>
-      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-800">
+      <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-coolGray-700">
         <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
-              <h6 className="uppercase text-gray-200 mb-1 text-xs font-semibold">
+              <h6 className="uppercase text-coolGray-100 mb-1 text-xs font-semibold">
                 Overview
               </h6>
               <h2 className="text-white text-xl font-semibold">Sales value</h2>
