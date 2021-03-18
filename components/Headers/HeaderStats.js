aa4bc896508b4a383b3d7fac5c059b714606b88f
EINazare <emanuelioannazare@gmail.com>
2021-03-18 10:42:00 +0200

    refactor: update all

--- components/Headers/HeaderStats.js
+++ components/Headers/HeaderStats.js
@@ -8,7 +8,7 @@ export default function HeaderStats() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-gray-900 md:pt-32 pb-32 pt-12">
+      <div className="relative bg-coolGray-800 md:pt-32 pb-32 pt-12">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div>
             {/* Card stats */}
