02bdc1d7de39abb4db9e02cd9b2c95c07e2136db
EINazare <emanuelioannazare@gmail.com>
2021-03-18 15:58:56 +0200

    style: run prettier

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
-          <link
-            rel="shortcut icon"
-            href="/img/brand/favicon.ico"
-          />
+          <link rel="shortcut icon" href="/img/brand/favicon.ico" />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
