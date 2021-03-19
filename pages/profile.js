451f97105edf0770f6c2a6345531cc2cd34b39ee
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:58:55 +0200

    refactor: change coolGray with blueGray

--- pages/profile.js
+++ pages/profile.js
@@ -35,13 +35,13 @@ export default function Profile() {
               y="0"
             >
               <polygon
-                className="text-coolGray-200 fill-current"
+                className="text-blueGray-200 fill-current"
                 points="2560 0 2560 100 0 100"
               ></polygon>
             </svg>
           </div>
         </section>
-        <section className="relative py-16 bg-coolGray-200">
+        <section className="relative py-16 bg-blueGray-200">
           <div className="container mx-auto px-4">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
               <div className="px-6">
@@ -58,7 +58,7 @@ export default function Profile() {
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
                       <button
-                        className="bg-coolGray-700 active:bg-coolGray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        className="bg-blueGray-700 active:bg-blueGray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
                       >
                         Connect
@@ -68,26 +68,26 @@ export default function Profile() {
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
                     <div className="flex justify-center py-4 lg:pt-4 pt-8">
                       <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-coolGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                           22
                         </span>
-                        <span className="text-sm text-coolGray-400">
+                        <span className="text-sm text-blueGray-400">
                           Friends
                         </span>
                       </div>
                       <div className="mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-coolGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                           10
                         </span>
-                        <span className="text-sm text-coolGray-400">
+                        <span className="text-sm text-blueGray-400">
                           Photos
                         </span>
                       </div>
                       <div className="lg:mr-4 p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-coolGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                           89
                         </span>
-                        <span className="text-sm text-coolGray-400">
+                        <span className="text-sm text-blueGray-400">
                           Comments
                         </span>
                       </div>
@@ -95,26 +95,26 @@ export default function Profile() {
                   </div>
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-coolGray-700 mb-2">
+                  <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
                     Jenna Stones
                   </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-coolGray-400 font-bold uppercase">
-                    <i className="fas fa-map-marker-alt mr-2 text-lg text-coolGray-400"></i>{" "}
+                  <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                    <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>{" "}
                     Los Angeles, California
                   </div>
-                  <div className="mb-2 text-coolGray-600 mt-10">
-                    <i className="fas fa-briefcase mr-2 text-lg text-coolGray-400"></i>
+                  <div className="mb-2 text-blueGray-600 mt-10">
+                    <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
                     Solution Manager - Creative Tim Officer
                   </div>
-                  <div className="mb-2 text-coolGray-600">
-                    <i className="fas fa-university mr-2 text-lg text-coolGray-400"></i>
+                  <div className="mb-2 text-blueGray-600">
+                    <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
                     University of Computer Science
                   </div>
                 </div>
-                <div className="mt-10 py-10 border-t border-coolGray-200 text-center">
+                <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-coolGray-700">
+                      <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
                         An artist of considerable range, Jenna the name taken by
                         Melbourne-raised, Brooklyn-based Nick Murphy writes,
                         performs and records all of his own music, giving it a
