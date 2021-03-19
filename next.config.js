4618fd499b1501873a39534d02a8bb30e909176a
EINazare <emanuelioannazare@gmail.com>
2021-03-19 12:57:54 +0200

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "/notus-nextjs"
-      : ""
+  process.env.NODE_ENV === "production"
+  ? "/notus-nextjs"
+  : "",
+  images: {
+    loader: 'imgix',
+    path: 'https://demos.creative-tim.com/notus-nextjs/',
+  },
 };
