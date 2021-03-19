e36b1658f49237b70874240d6ab0d0357a82d8c3
EINazare <emanuelioannazare@gmail.com>
2021-03-19 11:58:26 +0200

    refactor: change coolGray with blueGray

--- components/Cards/CardStats.js
+++ components/Cards/CardStats.js
@@ -17,10 +17,10 @@ export default function CardStats({
         <div className="flex-auto p-4">
           <div className="flex flex-wrap">
             <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-              <h5 className="text-coolGray-400 uppercase font-bold text-xs">
+              <h5 className="text-blueGray-400 uppercase font-bold text-xs">
                 {statSubtitle}
               </h5>
-              <span className="font-semibold text-xl text-coolGray-700">
+              <span className="font-semibold text-xl text-blueGray-700">
                 {statTitle}
               </span>
             </div>
@@ -35,7 +35,7 @@ export default function CardStats({
               </div>
             </div>
           </div>
-          <p className="text-sm text-coolGray-400 mt-4">
+          <p className="text-sm text-blueGray-400 mt-4">
             <span className={statPercentColor + " mr-2"}>
               <i
                 className={
