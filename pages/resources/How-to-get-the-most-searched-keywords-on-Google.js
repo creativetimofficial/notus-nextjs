8528c8b9a4140911999d559b360c856d9630c281
tunde <icepaigez@gmail.com>
2023-01-03 16:13:00 +0100

    first blog post
    Added content for a blog post

--- pages/resources/How-to-get-the-most-searched-keywords-on-Google.js
+++ pages/resources/How-to-get-the-most-searched-keywords-on-Google.js
@@ -100,14 +100,13 @@ const ToolsPage = () => {
 						introduce people to your product or service?
 					</p>
 					<p>
-						That is what this article is about; I am going to introduce this template that automatically
+						That is what this article is about; I am going to introduce a template that automatically
 						captures these questions for you in a Google Sheet, so that you can scale your question-based keyword research 
 						and create content for the people searching for these terms.
 					</p>
 				</section>
 				<section className="art-2">
 					<h1>Before then, see reasons why question-based keywords are a gold-mine of quality traffic!!</h1>
-					<img src="/img/Google Autosuggest Questions Screenshot.png"/>
 					<h2>1. Low competition</h2>
 					<p>
 						Typically, question-based keywords have much less competition 
@@ -126,6 +125,46 @@ const ToolsPage = () => {
 					<p>
 						This provides a great opportunity for you to answer their specific question 
 						and then have the chance to put forward your product or service as the solution to their problem.
+					</p>
+				</section>
+				<section className="art-3">
+					<h1>Question-based Keyword Research Google Sheets Template</h1>
+					<img src="/img/Google Autosuggest Questions Screenshot.png"/>
+					<p>
+						It sure takes a lot of time and effort to conduct research for 
+						the millions of actual, unfiltered questions people are typing into Google, same as the one above.
+					</p>
+					<p>
+						Wouldn't it be great if you could automate this process?
+					</p>
+					<img src="/img/keywordResearchGIF.gif"/>
+					<p>
+						<a>This question-based keyword Google Sheets template</a> accepts <strong>2-word</strong> head terms and then scans
+						Google for all the actual questions that people are searching related to the terms.
+					</p>
+					<img src="/img/question-words.png"/>
+					<p>
+						Since people ask questions using certain qualifiers, it uses a combination of these qualifiers and copies all of the possible
+						available questions into a new sheet, named after the head term.  
+					</p>
+					<p>
+						Once you have all the possible questions people are asking related to the 
+						head terms, you can go ahead to create content that provides the best possible 
+						answer to the questions and you will be sure to get highly targeted traffic for  
+						those terms. 
+					</p>
+					{
+					// 	<p>
+					// 	To get access to this template and scale your question-based keyword research, click here.
+					// </p>
+				}
+				</section>
+				<section className="art-4">
+					<h2>Conclusion</h2>
+					<p>
+						In summary, this Google Sheets Keyword Research template is a
+						valuable asset if you are trying to find keywords that can rank high 
+						in the search results and attract targeted traffic.  
 					</p>
 				</section>
 			</div>
