6252be6830e895d1e279b9ad6238c961e8bdad9e
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2023-01-18 21:49:34 +0700

    Change pictures and some structure

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <>
-            <footer className="relative bg-blueGray-200 pt-8 pb-6">
+            <footer className="relative bg-white pt-8 pb-6">
                 <div
                     className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
                     style={{transform: "translateZ(0)"}}
@@ -18,7 +18,7 @@ export default function Footer() {
                         y="0"
                     >
                         <polygon
-                            className="text-blueGray-200 fill-current"
+                            className="text-white fill-current"
                             points="2560 0 2560 100 0 100"
                         ></polygon>
                     </svg>
