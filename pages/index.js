fded03a4e476b977073762f41349fe8ef5f30ff3
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2023-01-25 18:02:52 +0700

    Export whatsapp link issues

--- pages/index.js
+++ pages/index.js
@@ -617,14 +617,14 @@ export default function Index() {
                                             Pintu Masuk
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_1.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_1.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
 
@@ -633,14 +633,14 @@ export default function Index() {
                                             Ruang Depan
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_2.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_2.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
 
@@ -649,14 +649,14 @@ export default function Index() {
                                             Ruang Tunggu
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_3.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_3.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
                                 </div>
@@ -667,14 +667,14 @@ export default function Index() {
                                             Peralatan Praktik
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_4.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_4.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
 
@@ -683,14 +683,14 @@ export default function Index() {
                                             Ruang Praktik
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_5.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_5.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
 
@@ -699,14 +699,14 @@ export default function Index() {
                                             Rak Peralatan
                                         </h5>
                                         <Link href="./tentang-kami">
-                                        <div
-                                            className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                                            <img
-                                                alt="..."
-                                                className="align-middle border-none max-w-full h-auto rounded-lg"
-                                                src="/img/photos/pic_6.png"
-                                            />
-                                        </div>
+                                            <div
+                                                className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                                                <img
+                                                    alt="..."
+                                                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                                                    src="/img/photos/pic_6.png"
+                                                />
+                                            </div>
                                         </Link>
                                     </div>
                                 </div>
